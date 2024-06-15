9499b6964060c4b617e4247459c3bfd113737a0f
nantawat chairat <nantawat@opsta.co.th>
2024-06-15 21:14:04 +0700

    add node

--- openfga-cli/source/model_test.py
+++ openfga-cli/source/model_test.py
@@ -7,13 +7,15 @@ from store import create_store, delete_store
 class TestAuthorizationModel(unittest.TestCase):
 
     def test_write_authorization_model(self):
+        # create a store
         store = asyncio.run(create_store("store"))
 
         # read file model.json and convert it to json
         with open('/home/app/openfga-cli/source/model.json', 'r') as file:
             json_model = file.read()
 
+        # write the authorization model to the store
         asyncio.run(write_authorization_model(store['id'], json_model))
-        # assert response is True
 
-        # asyncio.run(delete_store(store['id']))
+        # delete the store
+        asyncio.run(delete_store(store['id']))
