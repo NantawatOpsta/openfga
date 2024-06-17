88679ce0f55ced0e06e5b6b24a641de1dec5f6e0
nantawat chairat <nantawat@opsta.co.th>
2024-06-17 20:12:15 +0700

    chore: Update git credential helper path in .gitconfigt

--- openfga-cli/source/request.py
+++ openfga-cli/source/request.py
@@ -1,9 +1,10 @@
 from client import get_openfga_client
 from openfga_sdk.client.models.tuple import ClientTuple
 from openfga_sdk.client.models.write_request import ClientWriteRequest
+from openfga_sdk.client.models.check_request import ClientCheckRequest
 
 
-async def WriteRequestUser(
+async def WriteRequestCreate(
         store_id, authorization_model_id, user, relation, obj
 ):
     client = get_openfga_client(
@@ -25,3 +26,47 @@ async def WriteRequestUser(
     )
 
     return await client.write(body, options)
+
+
+async def WriteRequestDelete(
+        store_id, authorization_model_id, user, relation, obj
+):
+    client = get_openfga_client(
+        store_id=store_id, authorization_model_id=authorization_model_id
+    )
+
+    options = {
+        "authorization_model_id": authorization_model_id
+    }
+
+    body = ClientWriteRequest(
+        deletes=[
+            ClientTuple(
+                user=user,
+                relation=relation,
+                object=obj,
+            )
+        ]
+    )
+
+    return await client.write(body, options)
+
+
+async def WriteRequestCheck(
+    store_id, authorization_model_id, user, relation, obj
+):
+    client = get_openfga_client(
+        store_id=store_id, authorization_model_id=authorization_model_id
+    )
+
+    options = {
+        "authorization_model_id": authorization_model_id
+    }
+
+    body = ClientCheckRequest(
+        user=user,
+        relation=relation,
+        object=obj
+    )
+
+    return await client.check(body, options)
--- openfga-cli/source/request_test.py
+++ openfga-cli/source/request_test.py
@@ -2,15 +2,45 @@ import asyncio
 import unittest
 from store import create_store, delete_store
 from model import write_authorization_model
-from request import WriteRequestUser
+from request import WriteRequestCreate, WriteRequestDelete, WriteRequestCheck
 
 
 class TestRelation(unittest.TestCase):
 
+    def test_create_delete(self):
+        # create a store
+        store = asyncio.run(create_store("store"))
+
+        # read file model.json and convert it to json
+        with open('/home/app/openfga-cli/source/model.json', 'r') as file:
+            json_model = file.read()
+
+        # write the authorization model to the store
+        model = asyncio.run(write_authorization_model(store.id, json_model))
+
+        asyncio.run(WriteRequestCreate(
+            store.id,
+            model.authorization_model_id,
+            "user:admin",
+            "member",
+            "admin_group:admin"
+        ))
+
+        asyncio.run(WriteRequestDelete(
+            store.id,
+            model.authorization_model_id,
+            "user:admin",
+            "member",
+            "admin_group:admin"
+        ))
+
+        # delete the store
+        asyncio.run(delete_store(store.id))
+
     # add user1 tenant1 can view only tenant1
     # add user2 tenant2 can view only tenant2
     # add admin to admin_group can view all
-    def test_add_user_project_relation(self):
+    def test_user_check(self):
         # create a store
         store = asyncio.run(create_store("store"))
 
@@ -22,7 +52,7 @@ class TestRelation(unittest.TestCase):
         model = asyncio.run(write_authorization_model(store.id, json_model))
 
         # write the user project relation to the store
-        asyncio.run(WriteRequestUser(
+        asyncio.run(WriteRequestCreate(
             store.id,
             model.authorization_model_id,
             "user:user1",
@@ -30,5 +60,52 @@ class TestRelation(unittest.TestCase):
             "tenant:tenant1"
         ))
 
+        asyncio.run(WriteRequestCreate(
+            store.id,
+            model.authorization_model_id,
+            "user:user2",
+            "member",
+            "tenant:tenant2"
+        ))
+
+        asyncio.run(WriteRequestCreate(
+            store.id,
+            model.authorization_model_id,
+            "user:admin",
+            "member",
+            "admin_group:admin"
+        ))
+
+        # check if user1 can view tenant1
+        check_user_01 = asyncio.run(WriteRequestCheck(
+            store.id,
+            model.authorization_model_id,
+            "user:user1",
+            "can_view",
+            "tenant:tenant1"
+        ))
+        assert check_user_01.allowed is True
+
+        # check if user2 can view tenant2
+        check_user_02 = asyncio.run(WriteRequestCheck(
+            store.id,
+            model.authorization_model_id,
+            "user:user2",
+            "can_view",
+            "tenant:tenant2"
+        ))
+        assert check_user_02.allowed is True
+
+        # check if user2 can view tenant2
+        admin = asyncio.run(WriteRequestCheck(
+            store.id,
+            model.authorization_model_id,
+            "user:admin",
+            "can_view",
+            "tenant:tenant1"
+        ))
+        # assert admin.allowed is True
+        print(admin)
+
         # delete the store
-        asyncio.run(delete_store(store.id))
+        # asyncio.run(delete_store(store.id))
