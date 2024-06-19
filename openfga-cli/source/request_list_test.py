d83d0daf2e2bc84c11b84379eec6899f95a21d57
nantawat chairat <nantawat@opsta.co.th>
2024-06-19 15:37:35 +0700

    chore: Add test case for getting a list of projects a user can view

--- openfga-cli/source/request_list_test.py
+++ openfga-cli/source/request_list_test.py
@@ -65,3 +65,62 @@ class TestRequestList(unittest.TestCase):
 
         # delete the store
         asyncio.run(delete_store(store.id))
+
+    def test_get_list_member_in_project(self):
+        # create a store
+        store = asyncio.run(create_store("store"))
+
+        # read file model.json and convert it to json
+        with open('/home/app/openfga-cli/source/model.json', 'r') as file:
+            json_model = file.read()
+
+        # write the authorization model to the store
+        model = asyncio.run(write_authorization_model(store.id, json_model))
+
+        asyncio.run(RequestWrite(
+            store.id,
+            model.authorization_model_id,
+            "user:admin1",
+            "member",
+            "group:admin"
+        ))
+
+        asyncio.run(RequestWrite(
+            store.id,
+            model.authorization_model_id,
+            "group:admin#member",
+            "can_view",
+            "project:project1"
+        ))
+
+        asyncio.run(RequestWrite(
+            store.id,
+            model.authorization_model_id,
+            "group:admin#member",
+            "can_view",
+            "project:project2"
+        ))
+
+        asyncio.run(RequestWrite(
+            store.id,
+            model.authorization_model_id,
+            "group:admin#member",
+            "can_view",
+            "project:project3"
+        ))
+
+        list_of_projects = asyncio.run(RequestList(
+            store.id,
+            model.authorization_model_id,
+            "user:admin1",
+            "can_view",
+            "project"
+        ))
+
+        self.assertEqual(len(list_of_projects.objects), 3)
+        self.assertIn("project:project1", list_of_projects.objects)
+        self.assertIn("project:project2", list_of_projects.objects)
+        self.assertIn("project:project3", list_of_projects.objects)
+
+        # delete the store
+        asyncio.run(delete_store(store.id))
