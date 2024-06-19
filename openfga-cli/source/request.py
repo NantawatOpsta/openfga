0050bc3aee7bc97d071fbf7a9764a508f8264bd5
nantawat chairat <nantawat@opsta.co.th>
2024-06-19 16:31:30 +0700

    chore: Refactor request functions for consistency and clarity

--- openfga-cli/source/request.py
+++ openfga-cli/source/request.py
@@ -1,8 +1,11 @@
 from client import get_openfga_client
+from openfga_sdk.models.fga_object import FgaObject
 from openfga_sdk.client.models.tuple import ClientTuple
 from openfga_sdk.client.models.write_request import ClientWriteRequest
 from openfga_sdk.client.models.check_request import ClientCheckRequest
 from openfga_sdk.client.models.list_objects_request import ClientListObjectsRequest
+from openfga_sdk.client.models.list_users_request import ClientListUsersRequest
+from openfga_sdk.models.user_type_filter import UserTypeFilter
 
 
 async def RequestWrite(
@@ -91,3 +94,29 @@ async def RequestList(
     )
 
     return await client.list_objects(body, options)
+
+
+async def RequestListUser(
+    store_id,
+    authorization_model_id,
+    relation,
+    object_type,
+    object_id
+):
+    client = get_openfga_client(
+        store_id=store_id, authorization_model_id=authorization_model_id
+    )
+
+    options = {
+        "authorization_model_id": authorization_model_id
+    }
+
+    body = ClientListUsersRequest(
+        object=FgaObject(type=object_type, id=object_id),
+        relation=relation,
+        user_filters=[
+            UserTypeFilter(type="user"),
+        ]
+    )
+
+    return await client.list_users(body, options)
--- openfga-cli/source/request_list_test.py
+++ openfga-cli/source/request_list_test.py
@@ -2,7 +2,7 @@ import asyncio
 import unittest
 from store import create_store, delete_store
 from model import write_authorization_model
-from request import RequestWrite, RequestList
+from request import RequestWrite, RequestList, RequestListUser
 
 
 class TestRequestList(unittest.TestCase):
@@ -66,7 +66,7 @@ class TestRequestList(unittest.TestCase):
         # delete the store
         asyncio.run(delete_store(store.id))
 
-    def test_get_list_member_in_project(self):
+    def test_get_list_project_indirect_relation(self):
         # create a store
         store = asyncio.run(create_store("store"))
 
@@ -121,6 +121,64 @@ class TestRequestList(unittest.TestCase):
         self.assertIn("project:project1", list_of_projects.objects)
         self.assertIn("project:project2", list_of_projects.objects)
         self.assertIn("project:project3", list_of_projects.objects)
+
+        # delete the store
+        asyncio.run(delete_store(store.id))
+
+    def test_list_member_in_tenant(self):
+        # create a store
+        store = asyncio.run(create_store("store"))
+
+        # read file model.json and convert it to json
+        with open('/home/app/openfga-cli/source/model.json', 'r') as file:
+            json_model = file.read()
+
+        # write the authorization model to the store
+        model = asyncio.run(write_authorization_model(store.id, json_model))
+
+        asyncio.run(RequestWrite(
+            store.id,
+            model.authorization_model_id,
+            "user:admin1",
+            "member",
+            "group:admin"
+        ))
+
+        asyncio.run(RequestWrite(
+            store.id,
+            model.authorization_model_id,
+            "user:admin2",
+            "member",
+            "group:admin"
+        ))
+
+        asyncio.run(RequestWrite(
+            store.id,
+            model.authorization_model_id,
+            "user:admin3",
+            "member",
+            "group:admin"
+        ))
+
+        asyncio.run(RequestWrite(
+            store.id,
+            model.authorization_model_id,
+            "group:admin#member",
+            "can_view",
+            "tenant:tenant1"
+        ))
+
+        # list_of_members = asyncio.run(RequestListUser(
+        #     store.id,
+        #     model.authorization_model_id,
+        #     "can_view",
+        #     "tenant",
+        #     "tenant1"
+        # ))
+
+        # print(list_of_members)
+
+        # self.assertEqual(len(list_of_members.objects), 3)
 
         # delete the store
         asyncio.run(delete_store(store.id))
